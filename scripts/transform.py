4c9b8ec45964b9f1152046f324f653e6e3e91bb7
Vinit <vinitlohare06@gmail.com>
2026-03-26 12:59:17 +0530

    salary caterogy added

--- scripts/transform.py
+++ scripts/transform.py
@@ -2,7 +2,11 @@ import pandas as pd
 df = pd.read_csv('data/raw_data.csv')
 print(df)
 print("Raw data:")
-df['annual_salary']=df['salary'] * 12
+df['salary']=df['salary'] * 0.05
+# Add salary category
+df['salary_category'] = df['salary'].apply(
+    lambda x: 'Low' if x < 3000 else ('Medium' if x <= 5000 else 'High')
+)
 print("Transformed data")
 print(df)
 df.to_csv('data/processed_data.csv',index=False)
