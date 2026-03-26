9f7ef6c9f8aeb0ff31a6fb17703c50373dd1b2de
Vinit <vinitlohare06@gmail.com>
2026-03-26 12:31:40 +0530

    logic for the 5% salary

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 df = pd.read_csv('data/raw_data.csv')
-print("Raw data:")
 print(df)
-
-df['salary']=df['salary'] * 1.1
+print("Raw data:")
+df['salary']=df['salary'] * 0.05
+print("Transformed data")
+print(df)
 df.to_csv('data/processed_data.csv',index=False)
 print("Processed data saved successfully!")
