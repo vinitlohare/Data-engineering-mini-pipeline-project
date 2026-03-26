8f93677ed954de87d3ae3cc3da9910def423884b
Vinit <vinitlohare06@gmail.com>
2026-03-26 12:42:45 +0530

    annual salary logic

--- scripts/transform.py
+++ scripts/transform.py
@@ -2,7 +2,7 @@ import pandas as pd
 df = pd.read_csv('data/raw_data.csv')
 print(df)
 print("Raw data:")
-df['salary']=df['salary'] * 0.05
+df['annual_salary']=df['salary'] * 12
 print("Transformed data")
 print(df)
 df.to_csv('data/processed_data.csv',index=False)
